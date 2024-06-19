889085353d8e2308f52e9c0fc49b8d5d242e6bb5
nazrul-404 <156179297+nazrul-404@users.noreply.github.com>
2024-06-19 06:57:06 +0600

    Create 😘😘.js

---
+++ scripts/cmds/😘😘.js
@@ -0,0 +1,29 @@
+?cmd install 😘😘.js.js const fs = require("fs");
+module.exports.config = {
+	name: "😘",
+   version: "1.0.1",
+	role: 0,
+	author: "Nazrul",
+	description: "Voice No Prefix ",
+	category: "Voice",
+	usages: "??",
+    countDowns: 5, 
+};
+
+module.exports.onChat = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("😘")==0 ||    event.body.indexOf("💋")==0 ||
+  event.body.indexOf("😚")==0 ||    
+event.body.indexOf("😽")==0 || 
+ event.body.indexOf("😗")==0 || event.body.indexOf("😙")==0) {
+		var msg = {
+				body: "লুচ্চা পোলাপান এখানে চুম্মা চাপ্টি না করে পাট খেতে যাইয়া কর 😒",
+				attachment: fs.createReadStream(__dirname + `/Nazrul/cummah.mp3`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😘", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.onStart = function({ api, event, client, __GLOBAL }) {
+
+  }
