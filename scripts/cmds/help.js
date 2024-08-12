1b95f651c42f735b4a7aa6e417877abd18846d58
Hussainx22 <botx2025@gmail.com>
2024-08-12 21:57:54 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -62,32 +62,32 @@ module.exports = {
       const totalCommands = commands.size;
       msg += `\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆, 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗵𝗮𝘀 ${totalCommands} 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝘁𝗵𝗮𝘁 𝗰𝗮𝗻 𝗯𝗲 𝘂𝘀𝗲𝗱\n`;
       msg += `𝗧𝘆𝗽𝗲 ${prefix} 𝗵𝗲𝗹𝗽 𝗰𝗺𝗱𝗡𝗮𝗺𝗲 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 𝘁𝗵𝗮𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱\n\n`;
-      msg += `♥️ | Nazrul Project`; // its not decoy so change it if you want 
+      msg += `♥️ | Hussain Project`; // its not decoy so change it if you want 
 
       const helpListImages = [
-        'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',
-                          'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
+                          'https://i.imgur.com/HD50e5Z.jpeg',
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',                                    
-        'https://i.imgur.com/L3vE5Yv.jpeg', 
+        'https://i.imgur.com/HD50e5Z.jpeg',                                    
+        'https://i.imgur.com/HD50e5Z.jpeg', 
 
-        'https://i.imgur.com/L3vE5Yv.jpeg',
+        'https://i.imgur.com/HD50e5Z.jpeg',
       ];
 
       const helpListImage = helpListImages[Math.floor(Math.random() * helpListImages.length)];
