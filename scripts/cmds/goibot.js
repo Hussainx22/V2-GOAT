f9f46224a42af3f6952441c41ef140770ff97e30
Hussainx22 <botx2025@gmail.com>
2024-08-13 17:20:38 +0600

    Update goibot.js

--- scripts/cmds/goibot.js
+++ scripts/cmds/goibot.js
@@ -1,45 +1,223 @@
-Command executed successfully:
-const fs = require("fs-extra");
-const moment = require("moment-timezone");
+const axios = require("axios");
+module.exports.config = {
+  name: "goibot",
+  category: "Bot=one msg",
+  author: "Romim" // don't change this cuz this code really hard🙂
+}
+module.exports.onStart = async function({}) {}
+module.exports.onChat = async ({ api, event, args }) => {
+  const ApiUrl = await axios.get(`https://www.noobs-api.000.pe/dipto/baby?text=${Romim}`);
+  const R4M1M = ApiUrl.data.reply 
+const Romim = args.join(""); // don't touch this
+const Short = event.body.toLowerCase();
+  const msg = [`${R4M1M}`," আমরা দারুণ রকমের দুঃখ সাজাই প্রবল ভালোবেসে..!😅💔", " - আমি যখন একটু খুশি হওয়ার চেষ্টা করি, তখন দুঃখ এসে আবার আমাকে আঁকড়ে ধরে 😅💔", " °°অনুভূতি প্রকাশ করতে নেই মানুষ নাটক মনে করে মজা নেয়°..! 😥💔🥀", " কিছু মানুষ স্বল্প সময়ের জন্য আমাদের জীবনে আসে।কিন্তু দীর্ঘ সময় স্মৃতি রেখে যায়..!🙂💔", "𝙴𝙸 𝙿𝙰𝙶𝙾𝙻 𝙴𝚃𝙾 𝙳𝙰𝙺𝙾𝚂 𝙺𝙴𝙽?  ", " 𝙼𝚈𝙱 𝙸 𝙹𝚄𝚂𝚃 𝚆𝙰𝙽𝙽𝙰 𝙱𝙴 𝚈𝙾𝚄𝚁𝚂 ? 😌💝", " 𝙸 𝚂𝙰𝚈 𝙸 𝙻𝙾𝚅𝙴 𝚈𝙾𝚄 𝙵𝙾𝚁𝙴𝚅𝙴𝚁💝🐼", "𝙲𝙰𝙻𝙻 𝙼𝙴 𝙲𝙷𝙾𝙲𝙾𝙻𝙰𝚃𝙴 𝙽𝙾𝚃 𝙱𝙾𝚃 😢", " 𝚈𝚄𝙼𝙼𝚈 𝙱𝙱𝚈 𝚈𝙾𝚄 𝙰𝚁𝙴 𝚂𝙾 𝚂𝚆𝙴𝙴𝚃😋🤤 ", "𝙰𝚌𝙲𝙲𝙰𝙷 𝙱𝙾𝙻𝙾 𝙰𝙼𝙺𝙴 𝙻𝙰𝙶𝙱𝙴 𝙽𝙰𝙺𝙸 𝚁𝚄𝚂𝚂𝙸𝙰𝙽 ?", "তোর সাথে কথা নাই কারণ তুই অনেক লুচ্চা", " এইখানে লুচ্চামি করলে লাথি দিবো কিন্তু", "আমাকে চুমু দিবি 🫢🦋", "হেহে বাবু আমার কাছে আসো 😘💋", "আমি তোমাকে অনেক ভালোবাসি বাবু🥺💖", "your chocolate বট এর help list dekhte type koron *help", "কিরে বলদ তুই এইখানে 🙂", " আমাকেq চিনো না জানু? মনু", "hey bbe I'm your personal Based chatbot you ask me anything", "AR asbo na tor kache", "আমাকে ডাকলে ,আমি কিন্তু 𝐊𝐢𝐬𝐬 করে দিবো 😘", "Hop beda dakos kn🥲","-তাবিজ কইরা হইলেও ফ্রেম এক্কান করমুই তাতে যা হই হোক-🤧🥱"," ওই মামী আর ডাকিস না প্লিজ🥲"," "," হ্যা বলো, শুনছি আমি","বলো কি করতে পারি তোমার জন্য😌 ","𝐁𝐨𝐭 না জানু,বলো কারন আমি সিংগেল 😌 "," I love you tuna🥺🥶","Tuma dew xanu😍😘 "," এত কাছেও এসো না,প্রেম এ পরে যাবো তো 🙈"," দেখা হলে কাঠগোলাপ দিও..🤗","𝗕𝗲𝘀𝗵𝗶 𝗱𝗮𝗸𝗹𝗲 𝗮𝗺𝗺𝘂 𝗯𝗼𝗸𝗮 𝗱𝗲𝗯𝗮 𝘁𝗼__🥺 ","•-কিরে🫵 তরা নাকি  prem করস..😐🐸•আমারে একটা করাই দিলে কি হয়-🥺 ","Bolo Babu, তুমি কি আমাকে ভালোবাসো? 🙈💋 ","Single taka ki oporad🥺 "," Premer mora jole duve na😛","Ufff matha gorom kore disos😒 ","Boss hussain er chipay😜 ","bashi dakle boss hussain ke bole dimu😒 ","Xhipay atke gaci jan🥲 ","Xhipay atke gaci jan🥲 ","Washroom a🤣 ","bado maser kawwa police amar sawwa😞 ","I am single plz distrab me🥺🥲 ","𝗮𝗺𝗶 𝗯𝗼𝘁 𝗻𝗮 𝗮𝗺𝗮𝗸𝗲 𝗯𝗯𝘆 𝗯𝗼𝗹𝗼 𝗯𝗯𝘆!!😘 ","🍺 এই নাও জুস খাও..!𝗕𝗯𝘆 বলতে বলতে হাপায় গেছো না 🥲 ","🍺 এই নাও জুস খাও..!𝗕𝗯𝘆 বলতে বলতে হাপায় গেছো না 🥲 ","𝗛𝗶𝗶 𝗶 𝗮𝗺 𝗯𝗼𝘁 𝗰𝗮𝗻 𝗶 𝗵𝗲𝗹𝗽 𝘆𝗼𝘂!🤖 ","𝗲𝘁𝗼 𝗯𝗯𝘆 𝗯𝗯𝘆 𝗻𝗮 𝗸𝗼𝗿𝗲 𝘁𝗮𝗸𝗮 𝗼 𝘁𝗼 𝗽𝗮𝘁𝗵𝗮𝘁𝗲 𝗽𝗮𝗿𝗼😒🥳🥳 ","𝘁𝗼𝗿𝗲 𝗺𝗮𝗿𝗮𝗿 𝗽𝗿𝗲𝗽𝗲𝗿𝗮𝘁𝗶𝗼𝗻 𝗻𝗶𝗰𝗵𝗵𝗶...!!.🫡 ","𝘁𝗼𝗿𝗲 𝗺𝗮𝗿𝗮𝗿 𝗽𝗿𝗲𝗽𝗲𝗿𝗮𝘁𝗶𝗼𝗻 𝗻𝗶𝗰𝗵𝗵𝗶...!!.🫡 ","𝘂𝗺𝗺𝗮𝗵 𝗱𝗶𝗹𝗲 𝗹𝗼𝘃𝗲 𝘆𝗼𝘂 𝗸𝗼𝗺𝘂 𝗸𝗶𝗻𝘁𝘂😑 "," আমাকে ডাকলে ,আমি কিন্তু 𝐊𝐢𝐬𝐬 করে দিবো 😘"," Bal falaw😩","Tapraiya dat falai demu🥴 ","He🤤bolo amar jan kmn aso🤭 ","Hmmm jan ummmmmmah🫣 ","Chup kor nunu kete demu bashi dakle😬 ","Yes sir/mem😍 ","Assalamualikum☺️💖 ","Walaikumsalam😫🤓 ","Chaiya takos kn ki kobi kooo☹️ ","Onek boro beyadop re tui😒 ","Amar shate kew sex opps tex kore na😫 ","অনুমতি দিলাম-𝙋𝙧𝙤𝙥𝙤𝙨𝙚 কর বস HUSSAIN কে-🐸😾🔪 ","বলো কি বলবা, সবার সামনে বলবা নাকি?🤭🤏 ","-আজ একটা বিন নেই বলে ফেসবুকের নাগিন-🤧-গুলোরে আমার বস HUSSAIN ধরতে পারছে না-🐸🥲 "," তোর কি চোখে পড়ে না আমি ব্যাস্ত 😒💌","বলো কি বলবা, চিপায় যাইয়া বলবা নাকি সবার সামনে বলবা ?🤭🤏 "," 𝐁𝐨𝐭 না জানু,বলো কারন আমি সিংগেল 😌"," 𝐁𝐨𝐭 না জানু,বলো কারন আমি সিংগেল 😌"," দূরে যাইয়া মুরি খাঁ, তোর কোনো কাজ কাম নাই, শুধু 𝐁𝐨𝐭 𝐁𝐨𝐭 করিস  😒","আম গাছে আম নাই ঢিল কেন মারো, তোমার সাথে প্রেম নাই Bby কেন ডাকো🥲 ","𝘁𝗼𝗿 𝗻𝗮𝗻𝗶𝗿 𝗸𝗵𝗮𝗹𝗶 𝗴𝗵𝗼𝗿😒😒 ","এই এই তোর পরীক্ষা কবে ? শুধু Bot  Bot 𝗸𝗼𝗿𝗶𝘀 ","শুনবো না😼 তুমি আমাকে প্রেম করাই দাও নি🥺 পচা তুমি🥺 ","আগে একটা গান বলো, ☹ নাহলে কথা বলবো না 🥺 ","বলদা তোর কথা তোর বাড়ি কেউ শুনে না ,তো আমি কোনো শুনবো..?? 🤔 ","-𝙂𝙖𝙮𝙚𝙨-🤗-যৌবনের কসম দিয়ে আমারে 𝐁𝐥𝐚𝐜𝐤𝐦𝐚𝐢𝐥 করা হচ্ছে-🥲🤦‍♂🤧 ","জান হাঙ্গা করবা-🙊😝🌻 ","বেশি Bot Bot করলে leave নিবো কিন্তু😒😒 ","কি হলো ,মিস টিস করচ্ছিস নাকি🤣 "];
+  const A6Y = msg[Math.floor(Math.random() * msg.length)]
+  if (Short.startsWith("Bot") || Short.startsWith("bot") || Short.startsWith("A6Y")) {
+    api.sendMessage(`${A6Y}`, event.threadID, event.messageID)
+  }
+  if (Short.startsWith("Hussain") || Short.startsWith("Ariyan")) {
+    api.sendMessage("ki hoise amr boss ke khujo ken?", event.threadID, event.messageID);
+  }
+  if (Short.startsWith("love bot") || Short.startsWith("love bot")) {
+    return api.sendMessage("Hmm... Bot is too shy to love the bot admin :))", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("ummah") || Short.startsWith("bot ummah de")) {
+    return api.sendMessage("astagfirullah ki kos agula ", event.threadID, event.messageID);
+  };
 
-module.exports = {
-  config: {
-    name: "bot2",
-    version: "1.0",
-    author: "Nazrul",
-    countDown: 5,
-    role: 0,
-    description: "Bot",
-    category: "no prefix",
-    guide: {
-      en: "{p}{n}",
-    },
-  },
+  if (Short.startsWith("i love you ") || Short.startsWith("bot loves you")) {
+    return api.sendMessage("Hi, Bot loves you more than me, love bot <3", event.threadID, event.messageID);
+  };
 
-  onStart: async function ({}) {},
+  if (Short.startsWith("chagol bot") || Short.startsWith("dog bot")) {
+    return api.sendMessage("What dog just talked bad about me, want to die😠", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("chummma") || Short.startsWith("undercover cmm")) {
+    return api.sendMessage("Being disobedient to your biological parents, you say that's a broken person", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("Gorur bot") || Short.startsWith("gorur bot")) {
+    return api.sendMessage("Tui goru tor nana goru tor 14 gusti goru", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("pagol") || Short.startsWith("pagol bot")) {
+    return api.sendMessage("tor mathay latthi dibo baddop 😾 ",
+      event.threadID, event.messageID);
+  };
+  if (Short.startsWith("spam") || Short.startsWith("spam ditaso kn?")) {
+    return api.sendMessage("ore akon e kick deu 😾", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("bot koi") || Short.startsWith("baby tmi koi?")) {
+    return api.sendMessage("aije aije 🙋 ami ai dike 😋hum bolo 😻", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("baddop") || Short.startsWith("biyadopi")) {
+    return api.sendMessage("ei molla tui kinto biyadopi kortasos.😾", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("illiterate") || Short.startsWith("murkho ")) {
+    return api.sendMessage("Ho oi akta illiterate",
+      event.threadID, event.messageID);
+  };
+  if (Short.startsWith("কি করো") || Short.startsWith("Ki koro")) {
+    return api.sendMessage("gumai tumi?", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("dud night") || Short.startsWith("dud morning")) {
+    return api.sendMessage("nau dud khau", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("hehehhe") || Short.startsWith("hahahah")) {
+    return api.sendMessage("tui ato hastasos ken 😾", event.threadID, event.messageID);
+  };
 
-  onChat: async function ({ api, event, args, Threads, usersData }) {
-    const data = await usersData.get(event.senderID);
-    const name = data.name;
-    const { threadID, messageID, senderID } = event;
-    const time = moment.tz("Asia/Dhaka").format("HH:mm:ss L");
-    const idgr = `${event.threadID}`;
 
-    const messages = [
-- [ ]       "আমাকে ডাকলে ,আমি কিন্তু 𝐊𝐢𝐬𝐬 করে দিবো 😘 "Hop beda dakos kn🥲”Bal falaw😑”Tapraiya dat falai demu🥴”He🤤bolo amar jan kmn aso🤭”Hmmm jan ummmmmmah🫣”Chup kor nunu kete demu bashi dakle😬”Yes sir/mem🤗”Assalamualikum😊”Walaikumsalam😒🤓”Chaiya takos kn ki kobi kooo☹️”Amar shate kew sex opps tex kore na😫”Onek boro beyadop re tui😒”Tor khota kew sune na ami kn sunbo”অনুমতি দিলাম-𝙋𝙧𝙤𝙥𝙤𝙨𝙚 কর বস HUSSAIN কে-🐸😾🔪”বলো কি বলবা, সবার সামনে বলবা নাকি?🤭🤏”-আজ একটা বিন নেই বলে ফেসবুকের নাগিন-🤧-গুলোরে আমার বস HUSsAIN ধরতে পারছে না-🐸🥲”💌  আই লাভ ইউ.!!! আমার সোনা,ময়না,টিয়া 😍”💌  তোর কি চোখে পড়ে না আমি ব্যাস্ত 😒💌 “বলো কি বলবা, চিপায় যাইয়া বলবা নাকি সবার সামনে বলবা ?🤭🤏”💌  𝐁𝐨𝐭 না জানু,বলো কারন আমি সিংগেল 😌”💌  দূরে যাইয়া মুরি খাঁ, তোর কোনো কাজ কাম নাই, শুধু 𝐁𝐨𝐭 𝐁𝐨𝐭 করিস  😒”আম গাছে আম নাই ঢিল কেন মারো, তোমার সাথে প্রেম নাই BOT কেন ডাকো🥲”𝘁𝗼𝗿 𝗻𝗮𝗻𝗶𝗿 𝗸𝗵𝗮𝗹𝗶 𝗴𝗵𝗼𝗿😒😒”এই এই তোর পরীক্ষা কবে ? শুধু Bot  Bot 𝗸𝗼𝗿𝗶𝘀”শুনবো না😼 তুমি আমাকে প্রেম করাই দাও নি🥺 পচা তুমি🥺”আগে একটা গান বলো, ☹ নাহলে কথা বলবো না 🥺”💌  বলদা তোর কথা তোর বাড়ি কেউ শুনে না ,তো আমি কোনো শুনবো..?? 🤔”-𝙂𝙖𝙮𝙚𝙨-🤗-যৌবনের কসম দিয়ে আমারে 𝐁𝐥𝐚𝐜𝐤𝐦𝐚𝐢𝐥 করা হচ্ছে-🥲🤦‍♂🤧”জান হাঙ্গা করবা-🙊😝🌻”বেশি Bot Bot করলে leave নিবো কিন্তু😒😒”কি হলো ,মিস টিস করচ্ছিস নাকি🤣”-তাবিজ কইরা হইলেও ফ্রেম এক্কান করমুই তাতে যা হই হোক-🤧🥱🌻”ওই মামী আর ডাকিস না প্লিজ🥲”💌  হ্যা বলো, শুনছি আমি 😏”বলো কি করতে পারি তোমার জন্য😌”💌  𝐁𝐨𝐭 না জানু,বলো কারন আমি সিংগেল 😌”I love you🙂”Sawwa😝”Tuma dew ppy🥹😚”Ki hoice ko🤨”এত কাছেও এসো না,প্রেম এ পরে যাবো তো 🙈”দেখা হলে কাঠগোলাপ দিও..🤗”𝗕𝗲𝘀𝗵𝗶 𝗱𝗮𝗸𝗹𝗲 𝗮𝗺𝗺𝘂 𝗯𝗼𝗸𝗮 𝗱𝗲𝗯𝗮 𝘁𝗼__🥺”•-কিরে🫵 তরা নাকি  prem করস..😐🐸•আমারে একটা করাই দিলে কি হয়-🥺”Bolo Babu, তুমি কি আমাকে ভালোবাসো? 🙈💋”Bolo januu😋”Single taka ki oporad🥺”Premer mora jole duve na😛”Lucca🤨”Ufff matha gorom kore disos😒”Boss hussain er chipay😜”bashi dakle boss hussain ke bole dimu😒”Latti kabi🙂”W8 kor bal falai ekon😛🥸”Washroom a🤩”Ja gusol kor😟”Apner namaz er khota mone ase to?😊”bado maser kawwa police amar sawwa😞”I am single plz distrab me🥺🥲”ভাই তুই এত হাসিস না হাসলে তোরে চোরের মত লাগে-!!🌚🤣”𝗦𝗲𝗻𝘁𝗶 𝗲𝗺𝗼𝗷𝗶 𝗱𝗶𝗹𝗲🥺 𝗮𝘁𝗮 𝗱𝗶𝗯𝗲.....=😞”𝘀𝗲𝗻𝘁𝗶 𝗻𝗮 𝗸𝗵𝗮𝗶𝘆𝗮 𝟮𝟬 𝗸𝗵𝗮𝗼🙂”𝗮𝗺𝗶 𝗯𝗼𝘁 𝗻𝗮 𝗮𝗺𝗮𝗸𝗲 𝗯𝗯𝘆 𝗯𝗼𝗹𝗼 𝗯𝗯𝘆!!😘”🍺 এই নাও জুস খাও..!𝗕𝗯𝘆 বলতে বলতে হাপায় গেছো না 🥲)”𝗛𝗶𝗶 𝗶 𝗮𝗺 𝗯𝗼𝘁 𝗰𝗮𝗻 𝗶 𝗵𝗲𝗹𝗽 𝘆𝗼𝘂!🤖”𝗲𝘁𝗼 𝗯𝗯𝘆 𝗯𝗯𝘆 𝗻𝗮 𝗸𝗼𝗿𝗲 𝘁𝗮𝗸𝗮 𝗼 𝘁𝗼 𝗽𝗮𝘁𝗵𝗮𝘁𝗲 𝗽𝗮𝗿𝗼😒🥳🥳”𝗯𝗯𝘂 𝗮𝘃𝗮𝗯𝗲 𝘁𝗮𝗸𝗮𝗶𝗼 𝗻𝗮 𝗽𝗿𝗺 𝗮 𝗽𝗼𝗿𝗲 𝗷𝗮𝗯𝗼 𝘁𝗼🙈😒”𝘀𝗲𝗻𝘁𝗶 𝗸𝗵𝗮𝘀 𝗸𝗲𝗻 𝗯𝗼𝗹𝗼𝗱😘”𝘿𝙖𝙩 𝘽𝙚𝙧 𝙆𝙤𝙧𝙡𝙚 𝘿𝙖𝙩 𝙑𝙖𝙣𝙜𝙜𝙚 𝘿𝙞𝙗𝙤 😾”𝘁𝗼𝗿𝗲 𝗺𝗮𝗿𝗮𝗿 𝗽𝗿𝗲𝗽𝗲𝗿𝗮𝘁𝗶𝗼𝗻 𝗻𝗶𝗰𝗵𝗵𝗶...!!.🫡”𝗛𝗮𝘀𝗼 𝗸𝗲𝗻 𝗺𝘂𝗸𝗵𝗲 𝗺𝗼𝘀𝗮 𝗱𝗵𝘂𝗸𝗲 𝗷𝗮𝗯𝗲 𝘁𝗼!😦”𝘂𝗺𝗺𝗮𝗵 𝗱𝗶𝗹𝗲 𝗹𝗼𝘃𝗲 𝘆𝗼𝘂 𝗸𝗼𝗺𝘂 𝗸𝗶𝗻𝘁𝘂😑”
-    ];
 
-    const rand = messages[Math.floor(Math.random() * messages.length)];
+  if (Short.startsWith("let's go") || Short.startsWith("let's go")) {
+    return api.sendMessage("️1 is interaction, 2 is kick :))))", event.threadID, event.messageID);
+  };
 
-    if (event.body.toLowerCase() === "🐥" || event.body.toLowerCase() === "🐣") {
-      return api.sendMessage("-  𝐔𝐟𝐟'𝐬 𝐀𝐦𝐚𝐫 𝐁𝐚𝐛𝐮 𝐓𝐚🐥", "- ইসস আমার সোনা বাবু টা 🙊", threadID);
-    }
 
-    if (event.body.toLowerCase().startsWith("bot") || event.body.toLowerCase() === "Bot") {
-      const msg = {
-        body: `💌  ${rand}`,
-      };
-      return api.sendMessage(msg, threadID, messageID);
-    }
-  },
+
+  if (Short.startsWith("good night") || Short.startsWith("gd n8")) {
+    return api.sendMessage("️Sleep well ❤ Wish you all super nice dreams <3", event.threadID, event.messageID);
+  };
+
+
+  if (Short.startsWith("clmm bot")) {
+    return api.sendMessage("️Swear something dog 🙂 you've been holding on to you for a long time", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("bot  chi chi")) {
+    return api.sendMessage("️Swear something dog 🙂 you've been holding on to you for a long time", threadID);
+  };
+
+  if (Short.startsWith("cc bot")) {
+    return api.sendMessage("️Swear something dog 🙂 you've been holding on to you for a long time", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("dm bot")) {
+    return api.sendMessage("️Swear something to your dad :), you're a kid but you like to be alive :)", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("nobody loves me")) {
+    return api.sendMessage("️Come on, the bot loves you ❤ <3", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("does the bot love the admin bot")) {
+    return api.sendMessage("Yes, love him the most, don't try to rob me", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("bot im going") || Short.starstWith("bot im di")) {
+    return api.sendMessage("Im cc :))) m stop barking for me, but tell me im :>>", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("bot go away") || Short.startsWith("bot cut di")) {
+    return api.sendMessage("You're gone, your dad's gone, don't make you speak :))))", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("What's the bot swearing") || Short.startsWith("bot cursing")) {
+    return api.sendMessage("Damn you, shame on hahaha :>>, still asking", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("is the bot sad")) {
+    return api.sendMessage("Why can't I be sad because of everyone ❤ love you <3", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("does the bot love you")) {
+    return api.sendMessage("Yes I love you and everyone so much", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("bot goes to sleep")) {
+    return api.sendMessage("I'm a bot, you're the one who should go to sleep <3", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("has the bot eaten yet") || Short.startsWith("bot an comrade")) {
+    return api.sendMessage("I'm full when I see you eat <3", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("does the bot love me")) {
+    return api.sendMessage("Yes <3", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("does the bot have a brand") || Short.startsWith("does the bot fall")) {
+    return api.sendMessage("Yes <3", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("oh bot")) {
+    return api.sendMessage("Hurry, I have to serve other boxes :)", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("chup") || Short.startsWith("chup thak")) {
+    return api.sendMessage("️Amr Mukh, Amr iccha, Amr Mon. Tor ki bal,,,shala abal...ja vaag... 😒🙄",
+      event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("khabar khaiso?") || Short.startsWith("khaiso")) {
+    return api.sendMessage("️Nh...Tmk khabo.💖🥳", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("guyz") || Short.startsWith("guys")|| Short.startsWith("guy")) {
+    return api.sendMessage("️Don't Call Me Guys Bcz I AM Yours😊", event.threadID, event.messageID);
+  };
+  if (Short.startsWith("kiss") || Short.startsWith("kiss me")) {
+    return api.sendMessage("agula akon na biye r por 😋😻",
+      event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("wife") || Short.startsWith("bou")) {
+    return api.sendMessage("️Yes, My Husband🥰", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("bro")|| Short.startsWith("bbe")|| Short.startsWith("meye")) {
+    return api.sendMessage("️But I Am Girl, You Can Call Me baby😘", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("boy")) {
+    return api.sendMessage("️bbe, I Am Girl 😑", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("beb") || Short.startsWith("bebi")) {
+    return api.sendMessage("️hm Beb😚🖤", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("bby")) {
+    return api.sendMessage("️bolo Baby😚🖤", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("bebe")) {
+    return api.sendMessage("hmm Babe😚🖤", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("fight") || Short.startsWith("fyt")) {
+    return api.sendMessage("️Sorry, We Are Peace Lover ✌🏻🕊", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("hi") || Short.startsWith("hii") || Short.startsWith("hy")|| Short.startsWith("A6Y")|| Short.startsWith("a6y")) {
+    return api.sendMessage("️hi I'm A6Y and your personal assistant ask me anything ", event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("hello") || Short.startsWith("heloo")|| Short.startsWith("helu")) {
+    return api.sendMessage("️Hi And Wassup Preety Stranger🙂",
+      event.threadID, event.messageID);
+  };
+
+  if (Short.startsWith("bby") || Short.startsWith("baby")) {
+    const response = ApiUrl.data.reply;
+    api.sendMessage(`${response}`, event.threadID, (error, info) => {
+      global.GoatBot.onReply.set(info.messageID, {
+        commandName: this.config.name,
+        type: "reply",
+        messageID: info.messageID,
+        author: event.senderID,
+        msg: response,A6Y,
+      });
+    }, event.messageID);
+  };
+};
+module.exports.onReply = async ({ api, event,Reply}) => {
+  const respo = ApiUrl.data.reply
+  await api.sendMessage(`${respo}`, event.threadID, (error, info) => {
+    global.GoatBot.onReply.set(info.messageID, {
+      commandName: this.config.name,
+      type: "reply",
+      messageID: info.messageID,
+      author: event.senderID,
+      msg: respo,A6Y,
+    });
+  }, event.messageID);
 };
